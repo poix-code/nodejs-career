936416fe205139e3da1e44bdf094f135abfc9065
poix-code <1996@holbertonschool.com>
2021-05-18 05:53:27 -0500

    Testing EJS and Express server

--- server-test-express/index.js
+++ server-test-express/index.js
@@ -1,11 +1,36 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send("Hello world");
+//Settings
+app.set('appName', 'Express-server');
+app.set('views', __dirname + '/views');
+app.set('viewEngine', 'ejs');
+
+//Middlewares
+app.use(morgan('combined'));
+
+app.use( (req, res, next) => {
+  console.log("Next middleware");
+  next();
 });
 
-app.listen(port, () => console.log(`Server running on Port: ${port}`));
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/login', (req, res) => {
+  res.send("Login here!");
+});
+
+app.get('*', (req, res) => {
+  res.send("Route not found");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on Port: ${port}`)
+  console.log(`App name: ${app.get('appName')}`);
+});
